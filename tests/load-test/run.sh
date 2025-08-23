1210cd97bfbd13f403818aeab5f138a8ebd4d2cc
raimdev <raimbek.17@gmail.com>
2025-08-23 19:37:24 +0500

    infra

--- tests/load-test/run.sh
+++ tests/load-test/run.sh
@@ -2,8 +2,11 @@
 
 set -e
 
+# api_url="http://localhost:8081"
+api_url="https://bulbul.hub.hackload.kz"
+
 docker run --network host --rm -i -u $(id -u) \
-    -e API_URL=http://localhost:8081 \
+    -e "API_URL=${api_url}" \
     -e BASIC_AUTH=YXlzdWx0YW5fdGFsZ2F0XzFAZmVzdC50aXg6LzhlQyRBRD4= \
     -v $PWD:/app \
     -w /app \
